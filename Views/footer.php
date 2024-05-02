49c6fdbaf3dd9227b06d67cb28cc478f1f7a2ed8
Aligern <sasso.luca93@gmail.com>
2024-05-02 13:41:40 +0200

    first commit

---
+++ Views/footer.php
@@ -0,0 +1,7 @@
+
+<footer>
+    <h3>Powered by Luca "Aligern" Sasso</h3>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+include __DIR__ . "/Models/model.php";
+
+
+
+include __DIR__ ."/Views/header.php";
+?>
+
+
