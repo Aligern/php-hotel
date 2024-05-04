938d4e31d5e5c8f6b0192d37aa3601866ebbac96
Aligern <sasso.luca93@gmail.com>
2024-05-04 17:51:29 +0200

    login.php function add

--- Controllers/auth.php
+++ Controllers/auth.php
@@ -12,5 +12,13 @@ if (!empty($_POST['email']) && !empty($_POST['password'])) {
     # we need 'use($email, $password)' to use the variables outside the function
     $authenthicate = array_filter($users , function ($user) use($email, $password) {
         return $user['email'] == $email && $user['password'] == $password;
-    }); var_dump($authenthicate);
-};
+    }); // var_dump($authenthicate);
+    # we need an if to check if the user is already logged in
+    if (count($authenthicate) > 0) {
+        # array_shift remove and return the first element of the array, we may suppose that every user has only one email and one password and that the email and password are unique, so we can use it as a unique identifier
+        $user = array_shift($authenthicate);
+        $_SESSION['idUser'] = $user['id'];
+        $_SESSION['name'] = $user['name'];
+        header('Location: index.php');
+}
+}
